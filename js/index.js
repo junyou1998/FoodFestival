6e600a40496f69350d4c34074f79d727b736955e
陳俊佑 <adt106102@gm.ntcu.edu.tw>
2021-03-10 19:30:03 +0800

    update

--- js/index.js
+++ js/index.js
@@ -1,64 +1,84 @@
 var data = {
     index: 0,
     festivals: [{
-        date: '十月十七日',
-        day: 1,
-        festival: '春節',
-        good: '全家出遊、考取功名',
+        year: 2021,
+        month: 2,
+        date: '正月初一',
+        day: 12,
+        festival: '新年',
+        good: '吃年糕',
         bad: '穿著大紅、博弈下注'
     }, {
-        date: '十月十七日',
-        day: 20,
+        year: 2021,
+        month: 2,
+        date: '正月十五',
+        day: 26,
         festival: '元宵',
-        good: '吃好吃的元宵、躲在棉被',
+        good: '吃元宵、湯圓',
         bad: '穿著大紅、博弈下注'
     }, {
-        date: '十月十七日',
-        day: 25,
+        year: 2021,
+        month: 4,
+        date: '二月廿四',
+        day: 4,
         festival: '清明',
-        good: '掃墓',
+        good: '吃潤餅、紅龜粿',
         bad: '穿著大紅、博弈下注'
     }, {
-        date: '五月五日',
-        day: 15,
+        year: 2021,
+        month: 6,
+        date: '五月初五',
+        day: 14,
         festival: '端午',
-        good: '划龍舟、立蛋',
+        good: '吃粽子',
         bad: '跳水戲水'
     }, {
-        date: '五月五日',
-        day: 7,
+        year: 2021,
+        month: 8,
+        date: '七月初七',
+        day: 14,
         festival: '七夕',
-        good: '吃好吃的元宵、躲在棉被',
+        good: '吃餃子、麻花捲',
         bad: '跳水戲水'
     },  {
-        date: '五月五日',
-        day: 10,
+        year: 2021,
+        month: 8,
+        date: '七月十五',
+        day: 22,
         festival: '中元',
-        good: '划龍舟、立蛋',
+        good: '吃鴨子',
         bad: '穿著大紅、博弈下注'
     }, {
-        date: '五月五日',
-        day: 5,
+        year: 2021,
+        month: 2,
+        date: '八月十五',
+        day: 15,
         festival: '中秋',
-        good: '吃好吃的元宵、躲在棉被',
+        good: '吃月餅',
         bad: '跳水戲水'
     }, {
-        date: '五月五日',
-        day: 20,
+        year: 2021,
+        month: 9,
+        date: '九月初九',
+        day: 21,
         festival: '重陽節',
-        good: '划龍舟、立蛋',
+        good: '飲酒',
         bad: '穿著大紅、博弈下注'
     }, {
-        date: '五月五日',
-        day: 25,
+        year: 2021,
+        month: 12,
+        date: '十一月十八',
+        day: 21,
         festival: '冬至',
-        good: '划龍舟、立蛋',
+        good: '吃湯圓',
         bad: '跳水戲水'
     }, {
-        date: '五月五日',
-        day: 31,
+        year: 2021,
+        month: 2,
+        date: '十二月卅日',
+        day: 11,
         festival: '除夕',
-        good: '划龍舟、立蛋',
+        good: '吃長年菜',
         bad: '穿著大紅、博弈下注'
     }]
 }
@@ -99,7 +119,9 @@ var swiper = new Swiper(".swiper-container", {
     slidesPerView: 10,
     spaceBetween: 10,
     mousewheel: true,
-    centeredSlides: true
+    centeredSlides: true,
+    // touchRatio: 0,
+    // grabCursor: true,
 });
 
 var index = document.querySelectorAll(".swiper-slide").length;
---
+++ js/story.js
@@ -0,0 +1,163 @@
+let vm = new Vue({
+    el: '#app',
+    data: {
+        idx: 0,
+        now_index: 0,
+        lock: false,
+        festivals: [{
+            festival: '',
+            hero: {
+                img: '',
+                content: ''
+            },
+            stories: [{
+                img: '',
+                title: '',
+                content: ''
+            }]
+        }]
+    },
+    computed: {
+        festival: function () {
+            return this.festivals[this.idx]
+        }
+    },
+    created() {
+        window.onresize = this.resize
+    },
+    mounted() {
+        let festival = window.location.href.split('=')[1]
+        if(festival == undefined)
+            festival = 0
+        axios.get('../db.json')
+            .then((res) => {
+                this.festivals = res.data
+                this.idx = festival
+            })
+            .then(() => {
+                this.resize()
+                let texts = document.querySelectorAll('.text')
+                for (let index = 0; index < texts.length; index++) {
+                    const text = texts[index];
+                    text.onmouseover = function () {
+                        vm.lock = true
+                    }
+                    text.onmouseleave = function () {
+                        vm.lock = false
+                    }
+                }
+            })
+    },
+    methods: {
+        change(index) {
+            this.idx = index
+            this.now_index = 0
+            this.remove()
+            let story_blocks = document.querySelectorAll('.story_block')
+            story_blocks[this.now_index].classList.add('expend')
+            if(this.now_index>0)
+                story_blocks[this.now_index].querySelector('.title').classList.add('hide')
+
+        },
+        expend(index, event) {
+            this.now_index = index
+            this.remove()
+            event.currentTarget.classList.add('expend')
+            if(!event.currentTarget.querySelector('.title').classList.contains('first'))
+            event.currentTarget.querySelector('.title').classList.add('hide')
+            this.resize()
+        },
+        next(e) {
+            if (!this.lock) {
+                let story_blocks = document.querySelectorAll('.story_block')
+                if (e.deltaY > 0 && this.now_index < story_blocks.length - 1)
+                    this.now_index++
+                else if (e.deltaY < 0 && this.now_index > 0)
+                    this.now_index--
+                this.remove()
+                story_blocks[this.now_index].classList.add('expend')
+                if(this.now_index>0)
+                    story_blocks[this.now_index].querySelector('.title').classList.add('hide')
+
+            }
+        },
+        remove() {
+            let story_blocks = document.querySelectorAll('.story_block')
+
+            for (let index = 0; index < story_blocks.length; index++) {
+                const story_block = story_blocks[index];
+                story_block.classList.remove('expend')
+
+                story_blocks[index].querySelector('.title').classList.remove('hide')
+            }
+        },
+        resize() {
+            setTimeout(function () {
+                let w = document.querySelector('section').offsetWidth
+                let contents = document.querySelectorAll('.content')
+                let story_blocks = document.querySelectorAll('.story_block')
+                for (let index = 0; index < contents.length; index++) {
+                    const content = contents[index];
+                    content.style.width = w - 100 * (story_blocks.length) + 'px'
+                }
+            }, 300)
+        }
+    }
+})
+
+let mouseX = 0;
+let mouseY = 0;
+
+let ball = {
+    dom: document.querySelector(".cursor"),
+    x: 0,
+    y: 0,
+    speed: 0.2
+}
+
+let dot = {
+    dom: document.querySelector(".cursor-dot"),
+    x: 0,
+    y: 0,
+    speed: 1
+}
+
+function animate() {
+    let dist = {
+        x: mouseX - ball.x,
+        y: mouseY - ball.y
+    }
+    let dist1 = {
+        x: mouseX - dot.x,
+        y: mouseY - dot.y
+    }
+    ball.x = ball.x + (dist.x * ball.speed);
+    ball.y = ball.y + (dist.y * ball.speed);
+
+    dot.x = dot.x + (dist1.x * dot.speed);
+    dot.y = dot.y + (dist1.y * dot.speed);
+
+    ball.dom.style.left = ball.x + 'px';
+    ball.dom.style.top = ball.y + 'px';
+    dot.dom.style.left = dot.x + 'px';
+    dot.dom.style.top = dot.y + 'px';
+    requestAnimationFrame(animate)
+}
+animate();
+
+document.addEventListener('mousemove', function (e) {
+    mouseX = e.pageX;
+    mouseY = e.pageY;
+});
+
+let lists = document.querySelectorAll('li')
+
+for (let index = 0; index < lists.length; index++) {
+    const list = lists[index];
+    list.onmouseover = function () {
+        ball.dom.classList.add('grow')
+    }
+    list.onmouseleave = function () {
+        ball.dom.classList.remove('grow')
+    }
+}
